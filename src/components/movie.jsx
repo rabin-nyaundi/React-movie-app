8f72234c57ce0196e09f79bb3381aeac964157fd
nyaundi254 <nyaundirabin@gmail.com>
2020-10-21 18:34:07 +0300

    first commit

---
+++ src/components/movie.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { getMovies } from '../services/fakeMovieService'
+
+class Movie extends Component {
+    state = { 
+        movies: getMovies(),
+     }
+    render() { 
+        return <table className="table">
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Genre</th>
+                    <th>Number In Stock</th>
+                    <th>Daily Rental Rate</th>
+                </tr>
+            </thead>
+            <tbody>
+                {this.state.movies.map(movie => (
+                    <tr>
+                        <td>{ movie.title }</td>
+                        <td>{ movie.genre.name }</td>
+                        <td>{ movie.numberInStock }</td>
+                        <td>{ movie.dailyRentalRate }</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>;
+    }
+}
+ 
+export default Movie;
